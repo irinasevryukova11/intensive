59409f95d054e31700f32bbcb796d98348237065
irinasevryukova11 <irina.sevryukova11@gmail.com>
2026-01-04 17:45:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Добавляем базовый путь для GitHub Pages
+  // Базовый путь для GitHub Pages
   base: '/intensive/', 
   
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // Это более надежный способ указания пути
+      "@": resolve(__dirname, "./src"),
     },
   },
+  // Если ошибка повторится, мы попробуем еще более простой вариант
 })
